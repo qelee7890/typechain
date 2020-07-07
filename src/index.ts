0dd52d2cd83fd513e21f5394377556bdcb649cc0
QE <qe@QEui-MacBookPro.local>
2020-07-07 13:03:51 +0900

    create blockchain

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
-class Human {
-    public name: string;
-    public age: number;
-    public gender: string;
-    constructor(name: string, age: number, gender: string){
-        this.name = name;
-        this.age = age;
-        this.gender = gender;
+class Block {
+    public index: number;
+    public hash: string;
+    public previousHash: string;
+    public data: string;
+    public timestamp: number;
+    constructor(
+        index: number,
+        hash: string,
+        previousHash: string,
+        data: string,
+        timestamp: number
+    ){
+        this.index = index;
+        this.hash = hash;
+        this.previousHash = previousHash;
+        this.data = data;
+        this.timestamp = timestamp;
     }
 }
 
-const lynn = new Human("Lynn", 18, "female");
-const sayHi = (person: Human): string => {
-    return `Hello ${person.name}, you are ${person.age} old ${person.gender}.`;
-}
+const genesisBlock: Block = new Block(0, "2020202020202", "", "Hello", 123456);
 
-console.log(sayHi(lynn));
+let blockchain: [Block] = [genesisBlock];
+// only class Block can be pushed to the blockchain.
+// blockchain.push("stuff");
+
+console.log(blockchain)
 
 export {};
